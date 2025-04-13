511dcc797a0ccc8ecbf9830760527cbdf9572aaf
Yury123Poltaran <154461330+Yury123Poltaran@users.noreply.github.com>
2025-04-13 13:56:14 +0300

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,15 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  let total = days * costPerDay;
+
+  if (days >= 7) {
+    total -= 50;
+  } else if (days >= 3) {
+    total -= 20;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
+
